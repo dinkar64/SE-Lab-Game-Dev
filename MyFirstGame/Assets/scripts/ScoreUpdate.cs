943d68a9fb560da999e65f6a31bd2468ffeedcb8
dinkar64 <54977048+dinkar64@users.noreply.github.com>
2021-01-26 14:38:21 +0530

    Waste Collection

---
+++ MyFirstGame/Assets/scripts/ScoreUpdate.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreUpdate : MonoBehaviour
+{
+    public Text scoreValue;
+    public int wastepoints;
+
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        UpdateValue();
+    }
+
+    void OnTriggerEnter2D (Collider2D other) {
+        if(other.gameObject.tag == "paper")
+        {
+        score += wastepoints;
+        UpdateValue();
+        }
+        else
+        {
+            score -= wastepoints*2;
+            UpdateValue();
+        }
+    }
+
+    // void OnCollisionEnter2D(Collision2D collision)
+    // {
+    //     if (collision.gameObject.tag == "plastic")
+    //     {
+    //         score -= wastepoints*2;
+    //         UpdateValue();
+    //     }
+    // }
+
+    // Update is called once per frame
+    void UpdateValue()
+    {
+        scoreValue.text = "Score: " + score;
+    }
+}
