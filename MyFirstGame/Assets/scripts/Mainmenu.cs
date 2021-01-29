315bab709e8cf330c30634e07f6f03cd862f41db
dinkar64 <54977048+dinkar64@users.noreply.github.com>
2021-01-29 13:35:53 +0530

    Fish and UI

---
+++ MyFirstGame/Assets/scripts/Mainmenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Mainmenu : MonoBehaviour
+{
+    public void playgame () {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1 );
+        //SceneManager.LoadScene(2);
+    }
+
+    public void goback () {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1 );
+        //SceneManager.LoadScene(2);
+    }
+}
